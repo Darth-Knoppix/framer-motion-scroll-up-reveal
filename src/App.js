e7f263f7084c5b28f70893b325476ae3478b5947
Seth Corker <seth.corker@gmail.com>
2019-11-04 08:50:07 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { motion } from "framer-motion";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <article className="info">
+        <header className="header">
+          <span className="subtitle">React Animation</span>
+          <h1 className="title">Reveal on Scroll-up</h1>
+        </header>
+        <section className="content">
+          <p>
+            Using Framer Motion, the actions bar at the bottom of the screen
+            only fades in when scrolled up and fades out whenever we scroll
+            down.
+          </p>
+        </section>
+      </article>
+      <Actions />
     </div>
+  );
+}
+
+function Actions() {
+  const [lastYPos, setLastYPos] = React.useState(0);
+  const [shouldShowActions, setShouldShowActions] = React.useState(false);
+
+  React.useEffect(() => {
+    function handleScroll() {
+      const yPos = window.scrollY;
+      const isScrollingUp = yPos < lastYPos;
+
+      setShouldShowActions(isScrollingUp);
+      setLastYPos(yPos);
+    }
+
+    window.addEventListener("scroll", handleScroll, false);
+
+    return () => {
+      window.removeEventListener("scrollend", handleScroll, false);
+    };
+  }, [lastYPos]);
+
+  return (
+    <motion.div
+      className="actions"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: shouldShowActions ? 1 : 0 }}
+      transition={{ opacity: { duration: 0.2 } }}
+    >
+      <button>
+        <span className="fas fa-share-square" />
+      </button>
+      <button>
+        <span className="fas fa-user" />
+      </button>
+      <button>
+        <span className="fas fa-times" />
+      </button>
+    </motion.div>
   );
 }
 
